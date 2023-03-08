fee0511866c8a72a42180461a2f5f85e701835a5
Avantika <99116667+Ucan2022@users.noreply.github.com>
2023-03-08 21:54:00 +0530

    src

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <h2>Food</h2>
+            <h4>Rs 10</h4>
+            <h2>Petrol</h2>
+            <h4>Rs 100</h4>
+            <h2>Movies</h2>
+            <h4>Rs 200</h4>
+        </div>
+    )
+}
+
+export default ExpenseItem
