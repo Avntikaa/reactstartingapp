5b38cfed2ad12dbb7295e3ed2f22df0b030cd90d
Avantika <99116667+Ucan2022@users.noreply.github.com>
2023-03-09 15:26:36 +0530

    usestate

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,8 +1,14 @@
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetail from './ExpenseDetail';
 import './ExpenseItem.css';
+import { useState } from 'react';
+
 const ExpenseItem=(props)=> {
-  // const clickhandle=()=>{console.log('clicked')}
+  const [price,setPrice]=useState(props.price);
+
+const updatePrice=()=>{
+setPrice('100');
+}
 const deleteExpense=(e)=>{
   const i=document.getElementById('maindiv');
 i.removeChild(e.target.parentElement);
@@ -10,9 +16,9 @@ i.removeChild(e.target.parentElement);
   return (
     <div className='expense-item'>
       <ExpenseDate date={props.date}/>
-     <ExpenseDetail title={props.item} price={props.price} category={props.category}/>
-     {/* <button onClick={clickhandle}>click me</button> */}
+     <ExpenseDetail title={props.item} price={price} category={props.category}/>
      <button onClick={deleteExpense}>DELETE</button>
+     <button onClick={updatePrice}>UPDATE</button>
     </div>
   );
 }
