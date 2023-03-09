273ee3cdc9e2a003b040813a8fc2e1b4fd958070
Avantika <99116667+Ucan2022@users.noreply.github.com>
2023-03-09 14:25:27 +0530

    delete

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -2,12 +2,17 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetail from './ExpenseDetail';
 import './ExpenseItem.css';
 const ExpenseItem=(props)=> {
-  
-
+  // const clickhandle=()=>{console.log('clicked')}
+const deleteExpense=(e)=>{
+  const i=document.getElementById('maindiv');
+i.removeChild(e.target.parentElement);
+}
   return (
     <div className='expense-item'>
       <ExpenseDate date={props.date}/>
      <ExpenseDetail title={props.item} price={props.price} category={props.category}/>
+     {/* <button onClick={clickhandle}>click me</button> */}
+     <button onClick={deleteExpense}>DELETE</button>
     </div>
   );
 }
