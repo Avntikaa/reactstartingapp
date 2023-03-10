8ae4d06feedae7913d5e13836c7d9e0ea4be6ea8
Avantika <99116667+Ucan2022@users.noreply.github.com>
2023-03-10 21:50:07 +0530

    diy

--- App.js
+++ App.js
@@ -5,20 +5,35 @@ import ExpenseForm from './components/ExpenseForm';
 import FirstPage from './components/FirstPage';
 const App=()=>{
       const[expensearr,setExpensearr]=useState([]);
+      const[openform,setOpenform]=useState(false);
+      const[openbtn,setOpenbtn]=useState(true);
 
   const getuserdata=(ele)=>{
-   
     setExpensearr((prevexpense)=>{
       return [ele,...prevexpense];
     });
+    setOpenform((prev)=>!prev);
+    setOpenbtn((prev)=>!prev);
   }
+
+  const openFormDiv=()=>{
+    setOpenform((prev)=>!prev);
+    setOpenbtn((prev)=>!prev);
+  }
+
+  
   return (
     <div>
-      <ExpenseForm getdata={getuserdata}/>
+    {openform &&
+      <ExpenseForm getdata={getuserdata} open={openform}/>}
+      {openbtn &&
+      <button id="btn1" onClick={openFormDiv}>ADD EXPENSE</button>}
           <FirstPage expense={expensearr}/>
+<input type='text' id='input1'/>
 
     </div>
     
+  
   )
   }
 export default App;
