d7a8d4c0e83a1d3741ce3eb4e6491af4a00656a2
Avantika <99116667+Ucan2022@users.noreply.github.com>
2023-03-10 16:41:26 +0530

    filteredd in action

--- FirstPage.js
+++ FirstPage.js
@@ -4,20 +4,27 @@ import './FirstPage.css'
 import ExpensesFilter from './ExpenseFilter';
 
 const FirstPage=(props)=>{
+    const arr=props.expense;
+
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
 
-  const arr=props.expense;
+const filteredExpenses = arr.filter((expense) => {
+  const dt = new Date(expense.Date)
+
+    return dt.getFullYear().toString() === filteredYear;
+  });
+
 
   return( 
   <div className='expenses'>
           <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
 
  {
- arr.map((item,index)=>{
+filteredExpenses.map((item,index)=>{
          return (
          <ExpenseItem key={index} item={item.title} date={item.Date} price={item.price} category={item.locationofExpense}/>)
      })
